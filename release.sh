8d8c47356d2178a2c6438fffcae4e3bfeccb4726
Daniel Kraus <krada@gmx.net>
2012-05-22 19:01:48 +0200

    Release 1.0.0

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@
 # extension into two archive files that contain the current
 # git tag as the version number.
 
-tar cvzf release/LinkTitles-`git describe --tags --abbrev=0`.tar.gz gpl-*.txt HISTORY LinkTitles.* --exclude '*~' --transform 's,^,LinkTitles/,'
+tar cvzf release/LinkTitles-`git describe --tags --abbrev=0`.tar.gz gpl-*.txt NEWS LinkTitles.* --exclude '*~' --transform 's,^,LinkTitles/,'
