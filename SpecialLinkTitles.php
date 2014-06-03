5bdb6cc773727ba865c1330bf5d6192c68581e1f
Daniel Kraus <krada@gmx.net>
2014-06-03 18:16:20 +0200

    Add license info to SpecialLinkTitles.php.

--- SpecialLinkTitles.php
+++ SpecialLinkTitles.php
@@ -1,4 +1,29 @@
 <?php
+/*
+ *      \file SpecialLinkTitles.php
+ *
+ *      Copyright 2012-2014 Daniel Kraus <krada@gmx.net> ('bovender')
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+ 
+  if ( !defined( 'MEDIAWIKI' ) ) {
+    die( 'Not an entry point.' );
+	}
+ 
 class SpecialLinkTitles extends SpecialPage {
 
 	function __construct() {
