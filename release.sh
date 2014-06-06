8e1548aa4806ddfdf5f592db2a3ad66d3703b836
Daniel Kraus <krada@gmx.net>
2014-06-06 06:02:49 +0200

    Fix broken release package.
    The license file, which is not version-controlled, got lost during branch
    switching and was not included in the 2.4.0 ZIP file.
    
    Improved the release script so that it aborts if tar has errors.

--- release.sh
+++ release.sh
@@ -14,11 +14,15 @@ FILENAME="release/LinkTitles-$1.tar.gz"
 
 # Pack the relevant files into at tarball, renaming the paths to include the
 # root path "LinktTitles".
-tar cvzf $FILENAME gpl-*.txt NEWS LinkTitles.* --exclude '*~' --transform 's,^,LinkTitles/,'
+tar cvzf $FILENAME gpl-*.txt README.md NEWS *.php --exclude '*~' --transform 's,^,LinkTitles/,'
 
-# Add the tarball to the repository, commit it, then tag the commit and push to origin.
-git add $FILENAME
-git commit -m "Release version $1"
-git tag -a $1 -m "Version $1."
-git push
-git push --tags
+if [[ $? -eq 0 ]]; then
+	# Add the tarball to the repository, commit it, then tag the commit and push to origin.
+	git add $FILENAME
+	git commit -m "Release version $1"
+	git tag -a $1 -m "Version $1."
+	git push
+	git push --tags
+else
+	echo "tar had errors, did not push."
+fi
