c8ae7f8143e5c8292173601ecae4bf792a83c73a
Daniel Kraus <krada@gmx.net>
2015-02-05 20:47:24 +0100

    Modify release script to not commit stuff.

--- release.sh
+++ release.sh
@@ -16,13 +16,13 @@ FILENAME="release/LinkTitles-$1.tar.gz"
 # root path "LinktTitles".
 tar cvzf $FILENAME gpl-*.txt README.md NEWS *.php --exclude '*~' --transform 's,^,LinkTitles/,'
 
-if [[ $? -eq 0 ]]; then
-	# Add the tarball to the repository, commit it, then tag the commit and push to origin.
-	git add $FILENAME
-	git commit -m --amend
-	git tag -a $1 -m "Version $1."
-	git push
-	git push --tags
-else
-	echo "tar had errors, did not push."
-fi
+# if [[ $? -eq 0 ]]; then
+# 	# Add the tarball to the repository, commit it, then tag the commit and push to origin.
+# 	git add $FILENAME
+# 	git commit -m --amend
+# 	git tag -a $1 -m "Version $1."
+# 	git push
+# 	git push --tags
+# else
+# 	echo "tar had errors, did not push."
+# fi
