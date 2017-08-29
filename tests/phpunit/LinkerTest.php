66cb7d07935f93105b6a890260a82505711e62b5
Daniel Kraus <bovender@bovender.de>
2017-08-29 14:48:20 +0200

    Add license comments to tests.

--- tests/phpunit/LinkerTest.php
+++ tests/phpunit/LinkerTest.php
@@ -1,5 +1,26 @@
 <?php
 /**
+ * Copyright 2012-2017 Daniel Kraus <bovender@bovender.de> ('bovender')
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @author Daniel Kraus <bovender@bovender.de>
+ */
+
+/**
  * Unit tests for the LinkTitles\Linker class.
  *
  * The test class is prefixed with 'LinkTitles' to avoid a naming collision
@@ -8,7 +29,6 @@
  * Ideally the test classes should be namespaced, but when you do that, they
  * will no longer be automatically discovered.
  *
- * @group bovender
  * @group Database
  */
 class LinkTitlesLinkerTest extends LinkTitles\TestCase {
